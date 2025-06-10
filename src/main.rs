4f7007159737ab8d4324a6036b29f25a52e02666
nrposner <nicolasposner@gmail.com>
2025-06-10 13:37:49 -0400

    created dummy slurm api call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+// This line includes the bindings file that build.rs generates.
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 fn main() {
-    println!("Hello, world!");
+    println!("Calling into the Slurm C library...");
+
+    // FFI calls to C are unsafe. We must wrap them in an unsafe block.
+    unsafe {
+        // slurm_api_version() is a simple function that returns the API version as an integer.
+        // It's a perfect, safe test case.
+        let version = slurm_api_version();
+        println!("Successfully called slurm_api_version().");
+        println!("Slurm API Version: {}", version);
+    }
 }
