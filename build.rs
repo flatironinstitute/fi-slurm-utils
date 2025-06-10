3c23a4b9bdb3b32683b25777a61e3ea436581391
nrposner <nicolasposner@gmail.com>
2025-06-10 13:37:38 -0400

    updated build process

--- build.rs
+++ build.rs
@@ -1,21 +1,32 @@
 use std::env;
 use std::path::PathBuf;
+use std::fs;
 
 fn main() {
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search=/path/to/lib");
+    // get the slurm SDK path 
+    let slurm_sdk_path = env::var("SLURM_SDK_PATH").expect("SLURM_SDK_PATH environment variable not set. Please set it to the 'output' directory in slurm_docker_rocky");
+
+    let sdk_path = fs::canonicalize(PathBuf::from(slurm_sdk_path))
+        .expect("Failed to find SLURM_SDK_PATH. Does the directory exist?");
+    
+    let lib_dir = sdk_path.join("lib");
+    let include_dir = sdk_path.join("include");
+
+    println!("cargo:rustc-link-search=native={}", lib_dir.display());
 
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
-    println!("cargo:rustc-link-lib=bz2");
+    println!("cargo:rustc-link-lib=slurm");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // configure and run bindgen
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        // We need to tell bindgen's internal clang where to find the Slurm headers.
+        .clang_arg(format!("-I{}", include_dir.display()))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
