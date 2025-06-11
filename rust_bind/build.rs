01ba700bddc41e4481546d5bc84468e57bdff7cd
Nicolas Posner <nposner@rustyamd2.cm.cluster>
2025-06-11 18:02:37 -0400

    added and updated contents of rust_bind

---
+++ rust_bind/build.rs
@@ -0,0 +1,29 @@
+// build.rs (Final Linux Node Version)
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to link against the 'slurm' library.
+    // The `module load` command will have already put its location
+    // in the linker's standard search path.
+    println!("cargo:rustc-link-lib=slurm");
+
+    // Tell cargo to rebuild if the wrapper header changes.
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // Run bindgen
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs directory
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
+
