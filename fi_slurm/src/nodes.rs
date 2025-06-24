d2fbdc65e304d0524425e275314233cf574ed051
Nicolas Posner <nposner@rustyamd2.cm.cluster>
2025-06-24 19:56:44 -0400

    removed debugging code

--- fi_slurm/src/nodes.rs
+++ fi_slurm/src/nodes.rs
@@ -331,17 +331,6 @@ impl Node {
             }
         };
 
-        // --- TEMPORARY DEBUGGING CODE ---
-        let gres_conf_str = unsafe {c_str_to_string(raw_node.gres)};
-        if !gres_conf_str.is_empty() {
-            let node_name = unsafe {c_str_to_string(raw_node.name)};
-            println!(
-                "SUCCESS: Node '{}' has gres_used: '{}'",
-                node_name,
-                gres_conf_str,
-            );
-        }
-
         let energy = if raw_node.energy.is_null() {
             None
         } else {
