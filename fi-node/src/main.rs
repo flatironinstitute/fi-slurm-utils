98c33eba834d493c7e120ad2736fabc619dcc6d4
nrposner <nicolasposner@gmail.com>
2025-08-07 14:36:06 -0400

    adding capacity to get sample job id

--- fi-node/src/main.rs
+++ fi-node/src/main.rs
@@ -50,18 +50,18 @@ fn main() -> Result<(), String> {
 
         let top_n = args.leaderboard.first().unwrap_or(&10);
 
-        let mut map: HashMap<String, (u32, u32)> = HashMap::new();
+        let mut map: HashMap<String, (u32, u32, u32)> = HashMap::new();
 
         let jobs_collection = jobs::get_jobs()?;
 
         jobs_collection.jobs.iter().for_each(|(_, job)| {
-            let usage = map.entry(job.user_name.clone()).or_insert((0, 0)); //(job.user_name, (job.num_nodes, job.num_cpus))
+            let usage = map.entry(job.user_name.clone()).or_insert((0, 0, job.job_id)); //(job.user_name, (job.num_nodes, job.num_cpus))
 
             usage.0 += job.num_nodes;
             usage.1 += job.num_cpus;
         });
 
-        let mut sorted_scores: Vec<(&String, &(u32, u32))> = map.iter().collect();
+        let mut sorted_scores: Vec<(&String, &(u32, u32, u32))> = map.iter().collect();
 
         sorted_scores.sort_by(|a, b| b.1.cmp(a.1));
 
@@ -69,9 +69,8 @@ fn main() -> Result<(), String> {
             let rank = position + 1;
             let padding = if rank > 9 { "" } else {" "}; // just valid for the first 100
             let (initial, surname) = user.split_at_checked(1).unwrap_or(("Dr", "Evil"));
-            println!("{}. {} {}. {} is using {} nodes and {} cores", rank, padding, initial.to_uppercase(), surname.to_uppercase(), score.0, score.1);
+            println!("{}. {} {}. {} is using {} nodes and {} cores, sample id: {}", rank, padding, initial.to_uppercase(), surname.to_uppercase(), score.0, score.1, score.2);
         }
-        
 
         return Ok(())
     }
